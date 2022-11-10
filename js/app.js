cba384f498e13b8ac8d4f889fb12d530112bc419
Ramarama2 <ramaalrmoosh11@gmail.com>
2022-11-10 17:14:08 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+
+wow = new WOW(
+    {
+    boxClass:     'animate__animated',      // default
+    animateClass: 'animated', // default
+    offset:       0,          // default
+    mobile:       true,       // default
+    live:         true        // default
+  }
+  ).init();
